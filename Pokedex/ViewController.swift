a13465f2efc2225813f0bdbbbd1dfd03309536e0
zzangmmz <leemj036@gmail.com>
2025-01-02 16:54:04 +0900

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by 이명지 on 1/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
