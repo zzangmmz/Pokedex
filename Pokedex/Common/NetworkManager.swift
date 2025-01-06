ef8d99a6c756a7a9700f2b116e756e1650aa4711
zzangmmz <leemj036@gmail.com>
2025-01-06 12:51:33 +0900

    [feat] 네트워크 에러 enum 정의, NetworkManager 싱글톤 패턴으로 정의

--- Pokedex/Common/NetworkManager.swift
+++ Pokedex/Common/NetworkManager.swift
@@ -4,7 +4,16 @@
 //
 //  Created by 이명지 on 1/2/25.
 //
+import Foundation
+import RxSwift
+
+enum NetworkError {
+    case invalidUrl
+    case dataFetchFail
+    case decodingFail
+}
 
 final class NetworkManager {
-    
+    static let shared = NetworkManager()
+    private init() {}
 }
