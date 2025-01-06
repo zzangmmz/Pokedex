0a5614c735cde5c2c9a11badb2c04e32d2a8f8fd
zzangmmz <leemj036@gmail.com>
2025-01-06 16:01:53 +0900

    [refacotr] PokemonInfo -> PokemonType 구조체 이름 수정

--- Pokedex/Model/PokemonType.swift
+++ Pokedex/Model/PokemonType.swift
@@ -1,12 +1,12 @@
 //
-//  PokemonInfo.swift
+//  PokemonType.swift
 //  Pokedex
 //
 //  Created by 이명지 on 1/6/25.
 //
 import Foundation
 
-struct PokemonInfo: Decodable {
+struct PokemonType: Decodable {
     let name: String
     let url: String
 }
